f4b2953bf21373d822c09723b10b67f4ee7b4e3c
Tommy Kebschull <tkebschull@chatmeter.com>
2023-10-23 14:14:45 -0700

    changes to make compatible for render deployment part 4

--- NBA_Bet_Kansas_St/settings.py
+++ NBA_Bet_Kansas_St/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+# Need dj_database_url for render.com deployment
 import dj_database_url
 from pathlib import Path
 import os
